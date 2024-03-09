7ef831d74e5380b583e5b661075b7288d7c0710a
Fredeau123 <76597292+Fredeau123@users.noreply.github.com>
2024-03-09 19:09:01 +0100

    update

--- exercice3 - reactApp/src/ui/ErrorPage/index.jsx
+++ exercice3 - reactApp/src/ui/ErrorPage/index.jsx
@@ -22,11 +22,11 @@ export default function ErrorPage() {
   return (
     <div className="flex flex-col relative justify-center items-center h-screen">
       <h1 className="text-6xl font-bold pb-2">Oops!</h1>
-      <p className="text-2xl font-light text-gray-400">J'ai mangé cette page</p>
+      <p className="text-2xl font-light text-gray-400">I ate this Page</p>
       <img className="w-80 pb-24" src="/monster.png" alt="cute monster" />
       <div className="duration-400 p-6" style={{ transition: '300ms', position: 'absolute', top: top, left: left, transform: 'translate(-50%, -50%)' }}>
         <button
-          className="bg-blue-600 py-2 px-6 text-white rounded-xl cursor-pointer"
+          className="bg-gradient-to-r from-cyan-400 to-indigo-400 py-2 px-6 text-white cursor-default rounded-xl cursor-pointer"
           onMouseEnter={moveButton}
         >
           Home
